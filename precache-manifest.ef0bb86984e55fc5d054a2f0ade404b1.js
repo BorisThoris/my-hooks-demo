8f4d89a5af44f731a6515e5b28fbef9049152bf3
Boris <modaxxx009@yahoo.com>
2019-09-25 16:54:58 +0300

    Updates

---
+++ precache-manifest.ef0bb86984e55fc5d054a2f0ade404b1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "307158354c88d46c4e1ac73870ae0161",
+    "url": "/react-hooks-demo/index.html"
+  },
+  {
+    "revision": "21d11a3cf005d1f3081a",
+    "url": "/react-hooks-demo/static/css/main.f534ceb0.chunk.css"
+  },
+  {
+    "revision": "51e0fa7dc9a9c12f844a",
+    "url": "/react-hooks-demo/static/js/2.bb630f33.chunk.js"
+  },
+  {
+    "revision": "21d11a3cf005d1f3081a",
+    "url": "/react-hooks-demo/static/js/main.b8a8b701.chunk.js"
+  },
+  {
+    "revision": "78b60d58282811f262b1",
+    "url": "/react-hooks-demo/static/js/runtime~main.9f88a39a.js"
+  },
+  {
+    "revision": "0261859aa0726ca8fc5f9d2d13e00c73",
+    "url": "/react-hooks-demo/static/media/background2.0261859a.png"
+  },
+  {
+    "revision": "1715b1a216821c548990d4f2a3c9be52",
+    "url": "/react-hooks-demo/static/media/breakPedal.1715b1a2.png"
+  },
+  {
+    "revision": "ccae098cc5e6bdf2964f1b8197abd31d",
+    "url": "/react-hooks-demo/static/media/flameSvg.ccae098c.svg"
+  },
+  {
+    "revision": "476db15f8658c5e130d78ef9062e521d",
+    "url": "/react-hooks-demo/static/media/gasPedal.476db15f.png"
+  },
+  {
+    "revision": "6f3eb0876b457806831094e92ce8923a",
+    "url": "/react-hooks-demo/static/media/image2vector.6f3eb087.svg"
+  },
+  {
+    "revision": "bfc96fe6b92924a7146c569c933f195b",
+    "url": "/react-hooks-demo/static/media/mountainsBackground.bfc96fe6.png"
+  },
+  {
+    "revision": "348de5c8c33518e49bccbd4737c0cd1d",
+    "url": "/react-hooks-demo/static/media/skyBackground.348de5c8.jpg"
+  },
+  {
+    "revision": "5e9cf3c45e7d43b409012fdded21233f",
+    "url": "/react-hooks-demo/static/media/wheel2vector.5e9cf3c4.svg"
+  }
+]);
